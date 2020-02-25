fd0b15681c3d3a200a14de947d0290f99d6afaae
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2020-02-25 19:38:53 +0100

    Add files via upload

---
+++ CH19/mathfunction.py
@@ -0,0 +1,15 @@
+import numpy as np
+for q in range(101):
+    v=(2**q)    
+    print("Size of nb to describe",q," qubits:","{:,}".format(v))
+
+
+print("Facebook in the near future:")
+s=(2**50)*500
+print("{:,}".format(v))
+
+
+
+
+
+      
